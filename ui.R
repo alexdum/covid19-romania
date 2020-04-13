1ba8d194f0cd75a97757108dc72f39d27eb0cc53
alexdum <alexandru.dumitrescu@gmail.com>
2020-04-13 20:06:07 +0300

    fixed top menu

--- ui.R
+++ ui.R
@@ -12,13 +12,14 @@ ui <- shinyUI(
     fluidPage(
       tags$head(
         includeHTML("google-analytics.html"),
+        #tags$style(type = "text/css", "body {padding-top: 70px;}"),
         tags$meta(charset = "UTF-8"),
         tags$meta(name = "description", content = "Relevant facts and statistics about COVID-19 spread in Romania."),
         tags$meta(name = "keywords", content = "COVID-19, Romania, spread maps, relevant graphs"),
         tags$meta(name = "author", content = "Alexandru Dumitrescu"),
         tags$meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
       ),
-      navbarPage("Romania COVID-19", collapsible = T, fluid = T,id = "tabs",
+      navbarPage("Romania COVID-19", collapsible = T, fluid = T,id = "tabs", position =  "fixed-top",
                  selected = "#about",
                  
                  # Statistics & Facts ------------------------------------------------------
