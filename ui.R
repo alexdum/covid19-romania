f0dd138c3265ce344a51e881c2ce24f5ec3cf4af
alexdum <alexandru.dumitrescu@gmail.com>
2020-04-18 12:05:06 +0300

    tidy code

--- ui.R
+++ ui.R
@@ -9,32 +9,33 @@ source("sections/ui_maps.R", local = T)
 ui <- shinyUI(
   
   ui <- function(req) { 
-    fluidPage(heme = "bootstrap.css",
-      tags$head(
-        includeHTML("google-analytics.html"),
-        tags$style(type = "text/css", "body {padding-top: 70px;}"),
-        tags$meta(charset = "UTF-8"),
-        tags$meta(name = "description", content = "Relevant facts and statistics about COVID-19 spread in Romania."),
-        tags$meta(name = "keywords", content = "COVID-19, Romania, spread maps, relevant graphs"),
-        tags$meta(name = "author", content = "Alexandru Dumitrescu"),
-        tags$meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
-      ),
-      useShinyjs(),
-      navbarPage("Romania COVID-19", collapsible = T, fluid = T, id = "tabs", position =  "fixed-top",
-                 selected = "#about",
-                 
-                 # Statistics & Facts ------------------------------------------------------
-                 facts_ui,
-                 
-                 # maps ------------------------------------------------------
-                maps_ui,
-                 
-                 # Spread network ----------------------------------------------------------
-                # spread_ui,
-                 
-                 # About -------------------------------------------------------------------
-                 about_ui
-      )
+    fluidPage(
+              tags$head(
+                includeHTML("google-analytics.html"),
+                tags$style(type = "text/css", "body {padding-top: 70px;}"),
+                # pentru leaflet
+                tags$meta(charset = "UTF-8"),
+                tags$meta(name = "description", content = "Relevant facts and statistics about COVID-19 spread in Romania."),
+                tags$meta(name = "keywords", content = "COVID-19, Romania, spread maps, relevant graphs"),
+                tags$meta(name = "author", content = "Alexandru Dumitrescu"),
+                tags$meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+              ),
+              useShinyjs(),
+              navbarPage("Romania COVID-19", collapsible = T, fluid = T, id = "tabs", position =  "fixed-top",
+                         selected = "#about",
+                         
+                         # Statistics & Facts ------------------------------------------------------
+                         facts_ui,
+                         
+                         # maps ------------------------------------------------------
+                         maps_ui,
+                         
+                         # Spread network ----------------------------------------------------------
+                         # spread_ui,
+                         
+                         # About -------------------------------------------------------------------
+                         about_ui
+              )
     )
   }
 )
