2cc74014f56e88cb6580ddc4ba86b0ed988f8fa8
alexdum <alexandru.dumitrescu@gmail.com>
2020-04-21 15:47:27 +0300

    update about section

---
+++ sections/about/about.md
@@ -0,0 +1,43 @@
+## Dashboard to present COVID-19 pandemic pread in Romania.
+
+Built in [R](https://www.r-project.org/) using [Shiny](https://shiny.rstudio.com/), this dashboard aims to provide accurate and relevant facts and statistics about COVID-19 spread in Romania.
+
+The dashboard is structured in two main sections:
+
+* [Key figures and plots](https://covid-19.shinyapps.io/romania/#facts)
+* [Relevant maps](https://covid-19.shinyapps.io/romania/#maps)
+
+
+### Tools
+
+* Visualisation: [shiny](https://shiny.rstudio.com/)
+* Map: [leaflet](https://rstudio.github.io/leaflet/) 
+* Chart: [dygraphs](https://rstudio.github.io/dygraphs/); [plotly](https://plot.ly/r/)
+* Table: [DT](https://rstudio.github.io/DT/)
+* Deployment: [shinyapps.io](https://www.shinyapps.io/)
+
+
+### Data source
+
+The analysis is based on data provided by  [covid19.geo-spatial.org.](https://covid19.geo-spatial.org/despre) .
+
+Note:
+
+* Active cases are computed as total confirmed - total recovered - total deaths.
+* Active critical cases are COVID-19 patients needing acute and intensive care in hospitals.
+
+### Source code:  
+
+Repository: [https://github.com/alexdum](https://github.com/alexdum/covid19-romania)  
+Report issue(s) [here](https://github.com/alexdum/covid19-romania/issues)
+
+
+### Creator:  
+
+Alexandru Dumitrescu <br/>
+Email: [alexandru.dumitrescu@gmail.com](mailto:alexandru.dumitrescu@gmail.com)  
+
+---
+
+(c) 2020 Alexandru Dumitrescu | [MIT License](https://github.com/alexdum/covid19-romania/blob/master/LICENSE) 
+
--- sections/ui_about.R
+++ sections/ui_about.R
@@ -1,34 +1,27 @@
 about_ui <- tabPanel("About",icon = icon("question-circle"), value = "#about", id = "#about",
                      
                      sidebarLayout(fluid = T,
-                       sidebarPanel(
-                         width = 2,
-                         h3(format(updd, "%b %d, %Y")),
-                         h4(paste(infect, "Cases"),  style = "color:red"),
-                         h4(paste(recov, "Recovered"), style = "color:green"),
-                         h4(paste(decs, "Deaths"), style = "color:#636363"),      
-                         # dateRangeInput("daterange", "Date range:",
-                         #                start = min(dats.nod),
-                         #                end   = max(dats.nod),
-                         #                min = as.character(min(dats.nod)),
-                         #                max = as.character(max(dats.nod))),
-                         
-                         # sliderInput("opacity", "Opacity", 0.8, min = 0.1,
-                         #             max = 1, step = .1),
-                         # submitButton("Apply Changes",icon("refresh")),
-                         
-                       ),
-                       mainPanel(
-                         p("This web site aims to provide accurate and relevant facts and statistics about COVID-19 spread in Romania."),
-                         p("The analysis is based on data provided by covid19.geo-spatial.org."),
-                         p("The dashboard is structured in two main sections:"),
-                         tags$ul(
-                           tags$li("key figures and plots:", tags$a(href="https://covid-19.shinyapps.io/romania/#facts",target="_blank", "https://covid-19.shinyapps.io/romania/#facts")), 
-                           tags$li("relevant maps:", tags$a(href="https://covid-19.shinyapps.io/romania/#maps",target="_blank", "https://covid-19.shinyapps.io/romania/#maps")), 
-                           
-                           
-                           
-                         )
-                       )
+                                   sidebarPanel(
+                                     width = 2,
+                                     h3(format(updd, "%b %d, %Y")),
+                                     h4(paste(infect, "Cases"),  style = "color:red"),
+                                     h4(paste(recov, "Recovered"), style = "color:green"),
+                                     h4(paste(decs, "Deaths"), style = "color:#636363"),      
+                                     # dateRangeInput("daterange", "Date range:",
+                                     #                start = min(dats.nod),
+                                     #                end   = max(dats.nod),
+                                     #                min = as.character(min(dats.nod)),
+                                     #                max = as.character(max(dats.nod))),
+                                     
+                                     # sliderInput("opacity", "Opacity", 0.8, min = 0.1,
+                                     #             max = 1, step = .1),
+                                     # submitButton("Apply Changes",icon("refresh")),
+                                     
+                                   ),
+                                   mainPanel(
+                                     includeMarkdown("sections/about/about.md")
+                                     
+                                     
+                                   )
                      )
-) 
+)
