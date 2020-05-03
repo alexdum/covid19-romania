a2360f7b52d7b366faffd4c936aaa4bc5c041195
alexdum <alexandru.dumitrescu@gmail.com>
2020-05-03 20:37:23 +0300

    add counties

--- sections/no2/no2_climate.R
+++ sections/no2/no2_climate.R
@@ -3,11 +3,11 @@
 omi.clim <- raster::stack("data/OMI-Aura/OMI-Aura_L3-OMNO2d_ymonmean_2015_2019.nc4")
 omi.clim <- omi.clim/10^15  
 
-omi.clim[omi.clim>8] <- 8
-omi.clim[omi.clim<2] <- 2
+omi.clim[omi.clim > 8] <- 8
+omi.clim[omi.clim < 2] <- 2
 
 # set color palette
-color_pal <- colorNumeric(c('#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a'),
+color_pal <- colorNumeric(rev(c('#0A0615','#190D3D','#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4')),
                           domain = c(1.9, 8.1),
                           na.color = "transparent")
 
@@ -18,16 +18,18 @@ map.clim <- leaflet(
   
   #ddTiles(group = "OSM ") %>%
   #addProviderTiles(providers$Stamen.Toner, group = "Toner (default)") %>%
-  addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+  addProviderTiles(providers$Stamen.TonerLite) %>%
   addRasterImage(omi.clim[[1]], colors = color_pal, opacity = .8)  %>%
   addProviderTiles(providers$Stamen.TonerLabels) %>%
+  addProviderTiles(providers$OpenMapSurfer.AdminBounds) %>%
   addEasyButton(easyButton(
     icon    = "glyphicon glyphicon-home", title = "Reset zoom",
-    onClick = JS("function(btn, map){ map.setView([46, 25], 6); }"))) 
-  #%>%
+    onClick = JS("function(btn, map){ map.setView([46, 25], 6); }")))
+#%>%
   # addLayersControl(
   #   baseGroups = c("Toner Lite")
-  # ) %>%
+  # ) 
+
   # addLegend(pal = color_pal, values = 2:8, position = "topright",
   #           title = "mol./cm2", opacity = 0.8)
 
@@ -39,8 +41,10 @@ observe({
   
   leafletProxy("no2map_clim") %>%
     clearImages() %>%
+    addProviderTiles(providers$Stamen.TonerLite) %>%
     addRasterImage(reactiveRaster(), colors = color_pal, opacity = .8) %>%
-    addProviderTiles(providers$Stamen.TonerLabels)
+    addProviderTiles(providers$Stamen.TonerLabels) %>%
+    addProviderTiles(providers$OpenMapSurfer.AdminBounds) 
 })
 
 
