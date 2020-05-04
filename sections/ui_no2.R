476fa9552dcf44ab5c5580566f6dfd4838e7ddbd
alexdum <alexandru.dumitrescu@gmail.com>
2020-05-04 20:06:20 +0300

    no2 monthly map

--- sections/ui_no2.R
+++ sections/ui_no2.R
@@ -9,7 +9,27 @@ no2_ui <- tabPanel("NO₂", icon = icon("industry"), value = "#maps_no2",
                                               
                                  ),
                                  mainPanel(
+                                   
+                                   
                                    tabsetPanel(id = "maps_no2clim",
+                                               tabPanel("NO₂ 2020",
+                                                        fluidRow(
+                                                          h3(textOutput("mapno2_act.text")),
+                                                          leafletOutput("no2map_act"),
+                                                          HTML('<strong style="font-size:12px"><center>10<sup>15</sup>molecules/cm<sup>2</sup></center></strong>'),
+                                                          HTML('<center><img src="no2_legend_clim.png" height=35 width="127"></center>'),
+                                                          sliderTextInput(
+                                                            inputId = "no2.act", 
+                                                            label = p(tags$b("Select year month")), 
+                                                            choices = format(dats.act, "%b %Y"), selected = format(dats.act, "%b %Y")[1], 
+                                                            grid = TRUE,
+                                                            width = "100%",
+                                                            animate = T
+                                                          ),
+                                                          helpText("Press the play button for animation.", 
+                                                                   style = "text-align:right;")
+                                                        )
+                                               ),
                                                tabPanel("NO₂ Climatology",
                                                         fluidRow(
                                                           h3("Satellite Observed Tropospheric NO₂ Concentration - montlhy multiannual means 2015 - 2019"),
@@ -31,10 +51,6 @@ no2_ui <- tabPanel("NO₂", icon = icon("industry"), value = "#maps_no2",
                                                
                                                tabPanel("Data",
                                                         includeMarkdown("sections/no2/no2_text.md")
-                                               ),
-                                               
-                                               tabPanel("NO₂ 2020",
-                                                        h4("TBA")
                                                )
                                                
                                    )
